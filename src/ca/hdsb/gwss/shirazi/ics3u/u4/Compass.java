27115c146f8d6ca4eb41e44bb71c01a23744cd71
1shirazimat <1shirazimat@gws03906.ihdsb.CA>
2016-11-14 13:22:41 -0500

    Finished Compass.java

--- src/ca/hdsb/gwss/shirazi/ics3u/u4/Compass.java
+++ src/ca/hdsb/gwss/shirazi/ics3u/u4/Compass.java
@@ -1,11 +1,18 @@
 /*
-
+Name: Matthew Shirazi
+Date:  Monday November 14, 2016
+Version 1.0
+Description:
+           This program lets a user enter a compass and then prints it in 
+sentence form.
 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ca.hdsb.gwss.shirazi.ics3u.u4;
+
+import java.util.Scanner;
 
 /**
  *
@@ -17,7 +24,69 @@ public class Compass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+
+        // OBJECTS
+        Scanner input = new Scanner(System.in);
+        boolean validDirection = true;
+        String compass;
+        String directionOne;
+        String directionTwo;
+
+        // INPUT
+        System.out.println("Enter Compass: ");
+        compass = input.nextLine();
+
+        // OUTPUT 
+        String One = compass.substring( 0, 1 );
+        String Two = compass.substring( 3 );
+        String number = compass.substring( 1 , 3 );
+        
+            switch (One) {
+                case "S":
+                    directionOne = "SOUTH";
+                    break;
+                case "N":
+                    directionOne = "NORTH";
+                    break;
+                case "W":
+                    directionOne = "WEST";
+                    break;
+                case "E":
+                    directionOne = "EAST";
+                    break;
+                default:
+                    validDirection = false;
+                    directionOne = "";
+                    break;
+            }
+            switch (Two) {
+                case "S":
+                    directionTwo = "SOUTH";
+                    break;
+                case "N":
+                    directionTwo = "NORTH";
+                    break;
+                case "W":
+                    directionTwo = "WEST";
+                    break;
+                case "E":
+                    directionTwo = "EAST";
+                    break;
+                default:
+                    validDirection = false;
+                    directionTwo = "";
+                    break;
+            }
+
+            if( validDirection == true ) {
+                System.out.println("Start facing " + directionOne + ". Turn " + number
+                + " degrees " + directionTwo);
+                
+            } else if ( ( "S".equals(directionOne) && "S".equals(directionTwo) ) ||
+                   ( "N".equals(directionOne) && "N".equals(directionTwo) ) ||
+                   ( "W".equals(directionOne) && "W".equals(directionTwo) ) ||
+                   ( "E".equals(directionOne) && "E".equals(directionTwo) ) ) {
+                System.out.println("Invalid data. Please try again.");
+            }
     }
-    
 }
