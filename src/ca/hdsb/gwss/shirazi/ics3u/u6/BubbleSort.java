4d4de6be2c5d92523f565a7d5f3d2633543afa84
1shirazimat <1shirazimat@gws03906.ihdsb.CA>
2016-12-02 13:18:21 -0500

    so

--- src/ca/hdsb/gwss/shirazi/ics3u/u6/BubbleSort.java
+++ src/ca/hdsb/gwss/shirazi/ics3u/u6/BubbleSort.java
@@ -1,9 +1,4 @@
 /*
-Name: Matthew Shirazi
-Date: Friday December 2, 2016
-Version 1.0
-Description:
-
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,37 +15,22 @@ public class BubbleSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    
+    int[] data = new int[] {10, 100, 999, 543, 25, 192, 459, 632, 1, 290};
 
-        int[] data = new int[]{10, 100, 999, 543, 25, 192, 459, 632, 1, 290};
-
-        System.out.format("%12s %2s %11s\n", "Before Swap", "|", "After Swap");
-        beforeBubbleSort(data);
-        swap(data);
+    System.out.format("%12s %2s %11s\n", "Before Swap", "|", "After Swap");
+    
+for ( int pass = 0; pass < data.length - 2; pass++ ) {
+    
+    for ( int c = 0; c < data.length - 1; c++ ) {
+    // IS THE ONE BELOW BIGGER? SMALLER? IF IT IS, SWAP
     }
-
-    public static void beforeBubbleSort(int[] data) {
-
-        for (int c = 0; c < data.length; c++) {
-            System.out.println( "     " + data[c] );
-        }
+}
+        
     }
-
-    public static void swap(int data[]) {
-
-        for (int pass = 0; pass < data.length - 2; pass++) {
-
-            for (int c = 0; c < data.length; c++) {
-                
-            int swap;
-                
-            if (data[c] > data[c+1]) {
-                swap = data[c];
-                data[c] = data[c+1];
-                data[c+1] = swap;
-                
-                swap++;
-        }
-      }
+    public static void bubbleSort() {
+        
+        
+        
     }
-  }
 }
