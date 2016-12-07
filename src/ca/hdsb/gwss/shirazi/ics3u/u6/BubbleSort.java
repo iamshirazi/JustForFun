34f05016b5478f8d6ab74f13a48949d3f1c25329
1shirazimat <1shirazimat@gws03906.ihdsb.CA>
2016-12-07 08:12:25 -0500

    Fixed BubbleSort and Finished SelectionSort

--- src/ca/hdsb/gwss/shirazi/ics3u/u6/BubbleSort.java
+++ src/ca/hdsb/gwss/shirazi/ics3u/u6/BubbleSort.java
@@ -4,7 +4,7 @@ Date: Thursday December 1, 2016
 Version 1.0
 Description:
            Swaps numbers and puts them in order from lowest to highest using the
-bubble method.
+Bubble Sort method.
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -44,19 +44,23 @@ public class BubbleSort {
     }
 
     public static void bubbleSort(int[] data) {
-        int passNumber = 0;
+        int swaps = 0;
+        int comps = 0;
         for (int pass = 0; pass < data.length - 1; pass++) {
-            for (int c = 0; c < (data.length - 1); c++) {
-                int swap;
+            for (int c = 0; c < (data.length - 1) - pass; c++) {
+                comps++;
                 if (data[c] > data[c + 1]) {
-                    swap = data[c];
-                    data[c] = data[c + 1];
-                    data[c + 1] = swap;
+                    swaps++;
+                    swap( data, c, c+1 );
                 }
             }
-            passNumber++;
-            display(data);
-            System.out.println("Number of Passes: " + passNumber);
         }
+        System.out.println( "SWAPS: " + swaps + " COMP: " + comps );
+    }
+    
+    public static void swap(int[] data, int i, int j) {
+        int swap = data[i];
+        data[i] = data[j];
+        data[j] = swap;
     }
 }
--- src/ca/hdsb/gwss/shirazi/ics3u/u6/SelectionSort.java
+++ src/ca/hdsb/gwss/shirazi/ics3u/u6/SelectionSort.java
@@ -1,4 +1,10 @@
 /*
+Name: Matthew Shirazi
+Date: Tuesday December 6, 2016
+Version 1.0
+Description:
+           This program sorts the random array of numbers in order from least to
+greatest using the Selection Sort method.
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,30 +21,54 @@ public class SelectionSort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
-        int[] data = new int[10];
+
+        int[] data = new int[100];
         for (int c = 0; c < data.length - 1; c++) {
             data[c] = (int) (Math.random() * 1000) + 1;
         }
         System.out.print("Data Before Swapping:");
-       // display(data);
+        display(data);
         System.out.println("______________________________________");
-       // selectionSort(data);
+        selectionSort(data);
         System.out.print("Data After Swapping:");
-       // display(data);
+        display(data);
     }
-    
-    public static void selectionSort( int[] data ) {
+
+    public static void selectionSort(int[] data) {
         
-        int swap = 0;
-        int maxValue = 0;
-        
-        for (int pass = 0; pass < data.length; pass++) {
-            for (int z = 0; z < data.length; z++) {
-                
-            
-                
+       // VARIABLES
+        int swaps = 0;
+        int comps = 0;
+        int passNumber = 0;
+        int indexMaxValue;
+
+        // DETERMINE MAX VALUE
+        for (int pass = 0; pass < data.length-1; pass++) {
+            indexMaxValue = 0;
+            for (int z = 1; z < data.length - pass; z++) {
+                comps++;
+                if (data[indexMaxValue] < data[z]) {
+                    indexMaxValue = z;
+                }
             }
-        }   
+            // SWAP
+            swaps++;
+            swap( data, indexMaxValue, data.length - 1 - pass );            
+        }
+        System.out.println( "SWAPS: " + swaps + " COMP: " + comps );
+    }
+
+    public static void swap(int[] data, int i, int j) {
+        int swap = data[i];
+        data[i] = data[j];
+        data[j] = swap;
+    }
+
+    public static void display(int[] data) {
+
+        for (int c = 0; c < data.length; c++) {
+            System.out.print(" " + data[c]);
+        }
+        System.out.println();
     }
 }
